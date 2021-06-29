847187e78ce27ef80f4c7301a9d5d02ffbb1b5c6
SeaIceNX <63960170+SeaIceNX@users.noreply.github.com>
2021-06-29 22:56:18 +0800

    Add files via upload

---
+++ ChainGather.lua
@@ -0,0 +1,111 @@
+function coal()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Coal",rx=0] ~~~ detect ~1~~ coal_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Coal",rx=0] ~~~ detect ~~1~ coal_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Coal",rx=0] ~~~ detect ~~~1 coal_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Coal",rx=0] ~~~ detect ~-1~~ coal_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Coal",rx=0] ~~~ detect ~~-1~ coal_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Coal",rx=0] ~~~ detect ~~~-1 coal_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("coal",1,1)
+
+function iron()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Iron",rx=0] ~~~ detect ~1~~ iron_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Iron",rx=0] ~~~ detect ~~1~ iron_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Iron",rx=0] ~~~ detect ~~~1 iron_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Iron",rx=0] ~~~ detect ~-1~~ iron_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Iron",rx=0] ~~~ detect ~~-1~ iron_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Iron",rx=0] ~~~ detect ~~~-1 iron_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("iron",1,1)
+
+function gold()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Gold",rx=0] ~~~ detect ~1~~ gold_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Gold",rx=0] ~~~ detect ~~1~ gold_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Gold",rx=0] ~~~ detect ~~~1 gold_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Gold",rx=0] ~~~ detect ~-1~~ gold_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Gold",rx=0] ~~~ detect ~~-1~ gold_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Gold",rx=0] ~~~ detect ~~~-1 gold_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("gold",1,1)
+
+function copper()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Copper",rx=0] ~~~ detect ~1~~ copper_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Copper",rx=0] ~~~ detect ~~1~ copper_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Copper",rx=0] ~~~ detect ~~~1 copper_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Copper",rx=0] ~~~ detect ~-1~~ copper_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Copper",rx=0] ~~~ detect ~~-1~ copper_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Raw Copper",rx=0] ~~~ detect ~~~-1 copper_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("copper",1,1)
+
+function Diamond()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Diamond",rx=0] ~~~ detect ~1~~ Diamond_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Diamond",rx=0] ~~~ detect ~~1~ Diamond_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Diamond",rx=0] ~~~ detect ~~~1 Diamond_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Diamond",rx=0] ~~~ detect ~-1~~ Diamond_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Diamond",rx=0] ~~~ detect ~~-1~ Diamond_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Diamond",rx=0] ~~~ detect ~~~-1 Diamond_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("Diamond",1,1)
+
+function Emerald()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Emerald",rx=0] ~~~ detect ~1~~ Emerald_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Emerald",rx=0] ~~~ detect ~~1~ Emerald_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Emerald",rx=0] ~~~ detect ~~~1 Emerald_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Emerald",rx=0] ~~~ detect ~-1~~ Emerald_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Emerald",rx=0] ~~~ detect ~~-1~ Emerald_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Emerald",rx=0] ~~~ detect ~~~-1 Emerald_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("Emerald",1,1)
+
+function lapis()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Lapis Lazuli",rx=0] ~~~ detect ~1~~ lapis_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Lapis Lazuli",rx=0] ~~~ detect ~~1~ lapis_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Lapis Lazuli",rx=0] ~~~ detect ~~~1 lapis_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Lapis Lazuli",rx=0] ~~~ detect ~-1~~ lapis_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Lapis Lazuli",rx=0] ~~~ detect ~~-1~ lapis_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Lapis Lazuli",rx=0] ~~~ detect ~~~-1 lapis_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("lapis",1,1)
+
+function redstone()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Redstone Dust",rx=0] ~~~ detect ~1~~ redstone_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Redstone Dust",rx=0] ~~~ detect ~~1~ redstone_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Redstone Dust",rx=0] ~~~ detect ~~~1 redstone_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Redstone Dust",rx=0] ~~~ detect ~-1~~ redstone_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Redstone Dust",rx=0] ~~~ detect ~~-1~ redstone_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Redstone Dust",rx=0] ~~~ detect ~~~-1 redstone_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("redstone",1,1)
+
+function quartz()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Nether Quartz",rx=0] ~~~ detect ~1~~ quartz_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Nether Quartz",rx=0] ~~~ detect ~~1~ quartz_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Nether Quartz",rx=0] ~~~ detect ~~~1 quartz_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Nether Quartz",rx=0] ~~~ detect ~-1~~ quartz_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Nether Quartz",rx=0] ~~~ detect ~~-1~ quartz_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Nether Quartz",rx=0] ~~~ detect ~~~-1 quartz_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("quartz",1,1)
+
+function gold_nugget()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Gold Nugget",rx=0] ~~~ detect ~1~~ nether_gold_ore 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Gold Nugget",rx=0] ~~~ detect ~~1~ nether_gold_ore 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Gold Nugget",rx=0] ~~~ detect ~~~1 nether_gold_ore 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Gold Nugget",rx=0] ~~~ detect ~-1~~ nether_gold_ore 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Gold Nugget",rx=0] ~~~ detect ~~-1~ nether_gold_ore 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Gold Nugget",rx=0] ~~~ detect ~~~-1 nether_gold_ore 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("gold_nugget",1,1)
+
+function netherite_scrap()
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Ancient Debris",rx=0] ~~~ detect ~1~~ ancient_debris 0 setblock ~1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Ancient Debris",rx=0] ~~~ detect ~~1~ ancient_debris 0 setblock ~~1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Ancient Debris",rx=0] ~~~ detect ~~~1 ancient_debris 0 setblock ~~~1 air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Ancient Debris",rx=0] ~~~ detect ~-1~~ ancient_debris 0 setblock ~-1~~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Ancient Debris",rx=0] ~~~ detect ~~-1~ ancient_debris 0 setblock ~~-1~ air 0 destroy')
+    runCmdEx('execute @e ~~~ execute @s[type=item,name="Ancient Debris",rx=0] ~~~ detect ~~~-1 ancient_debris 0 setblock ~~~-1 air 0 destroy')
+end
+schedule("netherite_scrap",1,1)
+
+print"[INFO] 连锁挖矿已加载!"
